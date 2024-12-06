71ed4c04e0e60c3dae252ca731a6b8628af3306e
Maja Petkova <majapetkova1@yahoo.com>
2024-12-06 14:51:53 +0100

    Add functionality for getting products by category

--- src/pages/products/Products.vue
+++ src/pages/products/Products.vue
@@ -1,7 +1,7 @@
 <script>
 // import { products } from '../../constants/products';
 
-import { getAllProducts } from '../../services/products';
+import { getAllProducts, getProducts } from '../../services/products';
 import Categories from './components/Categories.vue';
 import ProductCard from './components/ProductCard.vue';
 
@@ -17,18 +17,32 @@ export default {
       products: [],
     };
   },
-  computed: {
-    visibleProducts() {
-      return this.activeCategory === '' ? this.products : this.products.filter(x => x.category === this.activeCategory);
-    },
-  },
+  // computed: {
+  //   visibleProducts() {
+  //     return this.activeCategory === '' ? this.products : this.products.filter(x => x.category === this.activeCategory);
+  //   },
+  // },
   async created() {
     this.products = await getAllProducts();
     this.isLoading = false;
   },
+
   methods: {
-    onSelect(value) {
-      this.activeCategory = this.activeCategory === value ? '' : value;
+    async onSelect(value) {
+      const category = this.activeCategory === value ? '' : value;
+      this.activeCategory = category;
+
+      await this.loadProducts(category);
+    },
+    async loadProducts(category = '') {
+      this.isLoading = true;
+      if (category) {
+        this.products = await getProducts(category);
+      }
+      else {
+        this.products = await getAllProducts();
+      }
+      this.isLoading = false;
     },
   },
 };
@@ -39,8 +53,8 @@ export default {
     <Categories :active="activeCategory" @select="onSelect" />
   </div>
   <progress v-if="isLoading" />
-  <div v-else-if="visibleProducts.length > 0" class="products">
-    <ProductCard v-for="prod in visibleProducts" :key="prod.title + prod.id" :product="prod" />
+  <div v-else-if="products.length > 0" class="products">
+    <ProductCard v-for="prod in products" :key="prod.title + prod.id" :product="prod" />
   </div>
 </template>
 
--- src/services/products.js
+++ src/services/products.js
@@ -12,3 +12,14 @@ export async function getAllProducts() {
     return [];
   }
 }
+
+export async function getProducts(slug) {
+  try {
+    const response = await axiosDJ.get(`/${ENDPOINT}/category/${slug}`);
+    return response.data.products;
+  }
+  catch (err) {
+    console.error('Ooops unexpected', err);
+    return [];
+  }
+}
